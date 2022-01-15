f2eebcff67076b2d6d42b0294003fefe193f5d23
manishmeshram-in <69447989+manishmeshram-in@users.noreply.github.com>
2022-01-15 17:52:12 +0530

    File for code generator
    File for code generator

---
+++ src/main/java/com/code/generator/RunCommand3.java
@@ -0,0 +1,90 @@
+package com.code.generator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RunCommand3 {
+
+
+
+	final String CMD = "cmd.exe";
+	final String C = "/c";
+	final String CD = "cd \"";
+	String drive;
+	String [] folderHierarchy;
+	String [] commandList = {"dir"};
+
+	public String[] getCommandList() {
+		return commandList;
+	}
+
+	public void setCommandList(String[] commandList) {
+		this.commandList = commandList;
+	}
+
+	public RunCommand3(String drive, String [] folderHierarchy){
+		this.drive = drive.toUpperCase();
+	}
+
+	public RunCommand3(String drive, String [] folderHierarchy, String [] commandList){
+		this.drive = drive.toUpperCase();
+		this.folderHierarchy = folderHierarchy;
+		this.commandList = commandList;
+	}
+
+	public Process runCommand(){
+		
+		String folderPath = ":\\\\";
+		String commands = "";
+		
+		for (String folder :folderHierarchy) {
+			folderPath += folder + "\\\\";
+		}
+		System.out.println("folderPath: "+folderPath);
+		
+		for (String command :commandList) {
+			commands += " && " + command;
+		}
+		System.out.println("commands: "+commands);
+
+		//ProcessBuilder builder = new ProcessBuilder(CMD, C, "cd \"" + drive + ":\\Test\" && mkdir new3 && dir");
+		//ProcessBuilder builder = new ProcessBuilder(CMD, C, "cd \"" + drive + ":\\Test\\new3\" && mkdir new3 && dir");
+		//ProcessBuilder builder = new ProcessBuilder(CMD, C, "cd \"" + drive + folderPath + "\"" + " && mkdir new3 && dir");
+		//ProcessBuilder builder = new ProcessBuilder(CMD, C, "cd \"" + drive + folderPath + "\"" + commands);
+		ProcessBuilder builder = new ProcessBuilder(CMD, C, CD + drive + folderPath + "\"" + commands);
+
+		builder.redirectErrorStream(true);
+		Process p = null;
+
+		try {
+			p = builder.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return p;
+	}
+
+	public void printCommandOutput(Process p) {
+		BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String line;
+		try {
+			while (true) {
+				line = r.readLine();
+				if (line == null) {
+					break;
+				}
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void runCommandPrintingOutput() {
+		Process process = runCommand();
+		printCommandOutput(process);
+	}
+
+}
+
